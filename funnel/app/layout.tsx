ed7bb325a33154814a1c1ebf3e10718b19c279a3
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-05-18 19:33:41 +0300

    first commit

---
+++ funnel/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cash To Collect",
+  description: "Collect cash from your customers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
